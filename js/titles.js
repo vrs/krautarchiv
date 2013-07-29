99f52742349515a971096bbe64c81c06a05be5e6
lambda <lambda@lambda.name>
2013-07-29 21:04:57 +0100

    notes, misc

--- js/titles.js
+++ js/titles.js
@@ -2,7 +2,6 @@ window.addEvent('domready', function () {
   // TODO delegation (don't bother now)
   var tips = new Tips('a[title]', {
     className: 'tooltip',
-    offset: {x: 15, y: 1},
     text: null
   });
 });
