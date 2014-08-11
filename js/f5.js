65b6964fcb00f041deb509c3dfe91ff6bba51baf
anonymous <anonymous@anonymous>
2014-08-11 23:52:01 +0100

    f5.js: stub until threads can actually change

--- js/f5.js
+++ js/f5.js
@@ -8,5 +8,23 @@ settings.register({
   }],
 });
 
-if (settings.store.f5.enable) {
-}
+if (settings.store.f5.enable) { (function () {
+
+$$(document).addEvent(
+  'keydown',
+  function (ev) {
+    // stub, this actually needs sorting out of various idempotency issues
+    var thread;
+    if (ev.key === 'f5' && $$('main.thread').length) {
+      ev.preventDefault();
+      thread = new board.Thread($$('main.thread article.thread')[0]);
+      console.log('f5');
+      /*
+      thread.onLoad(function updateThread(newthread) {
+      });
+      thread.get();
+      */
+    }
+  });
+
+})() }
